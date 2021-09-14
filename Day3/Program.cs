0058ee0ecd6316fb2eadff36dac0a637b51440fa
Sarang <sarang11320@gmail.com>
2021-09-14 11:45:18 +0530

    Add project files.

---
+++ Day3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace day3task1
+{
+
+    class SwapNum
+    {
+        public void Swap(ref int x, ref int y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+
+        }
+
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            SwapNum obj = new SwapNum();
+            int a = 100;
+            int b = 200;
+            Console.WriteLine("before swapping");
+            Console.WriteLine("Num1 is {0}", a);
+            Console.WriteLine("Num2 is {0}", b);
+            Console.WriteLine("after swapping");
+            obj.Swap(ref a, ref b);
+            Console.WriteLine("Num1 is {0}", a);
+            Console.WriteLine("Num2 is {0}", b);
+
+
+
+
+        }
+    }
+}
